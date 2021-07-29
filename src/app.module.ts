d9585906b57cba464f555f869c1f223e2ecffc64
ribeKim <ribe@ribe.io>
2021-07-29 16:00:24 +0900

    connect database & add swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot(),
+      TypeOrmModule.forRoot({
+        type: 'mssql',
+        host: process.env.DBHost,
+        port: 1433,
+        username: process.env.DBUser,
+        password: process.env.DBPass,
+        database: process.env.DBName,
+        "entities": ["dist/**/*.entity{.ts,.js}"],
+        synchronize: true,
+        extra: {
+          trustServerCertificate: true,
+        },
+      }),
+      AuthModule,
+      UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
