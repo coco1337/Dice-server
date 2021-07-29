401a57fc54ad114be0a7045b524d916544122b13
ribeKim <ribe@ribe.io>
2021-07-29 15:59:49 +0900

    add users

---
+++ src/users/users.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({length: 100})
+  userId?: string;
+
+  @Column({length: 200})
+  password?: string;
+
+  @Column({length: 100, nullable: true})
+  uuid?: string;
+
+  @Column({length: 50, nullable: true})
+  ip?: string;
+
+  @Column({nullable: true})
+  joinDate?: Date;
+
+  @Column({nullable: true})
+  lastUpdate?: Date;
+
+  @Column({nullable: true})
+  lastLogin?: Date;
+}
