39fe1c184d1b6e275e65407faef79d71aacd170b
ribeKim <ribe@ribe.io>
2021-08-01 16:40:51 +0900

    user Validation 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,29 +1,36 @@
 import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import { LoginDto } from './dto/login-dto';
+import { User } from '../users/users.entity';
 
 @Injectable()
 export class AuthService {
   constructor(
       private usersService: UsersService,
-      private jwtService: JwtService
+      private jwtService: JwtService,
   ) {}
 
-  async validateUser(username: string, password: string): Promise<unknown> {
-    const user = await this.usersService.getAccount(username);
+  async validateUser(userInfo: LoginDto): Promise<User> {
+    const user = await this.usersService.getAccount(userInfo.username);
 
-    if (user && user.password === password) {
-      const { password, ...result } = user;
+    if (user && user.password === userInfo.password) {
+      const {password, ...result} = user;
       return result;
     }
 
     return null;
   }
 
-  async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+  async login(userInfo: LoginDto): Promise<object> {
+    const user = await this.validateUser(userInfo);
+
+    if (!user) return null;
+
+    const payload = {username: user.userId, sub: user.uuid};
+
     return {
       access_token: this.jwtService.sign(payload),
-    }
+    };
   }
 }
