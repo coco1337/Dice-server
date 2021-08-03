61964c35ee8fa33dfd5e72f9e72ad12dad12cb9b
ribeKim <ribe@ribe.io>
2021-08-03 11:28:21 +0900

    파일 이동

---
+++ src/users/entities/users.entity.ts
@@ -0,0 +1,28 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column({ type: 'varchar', length: 100 })
+  userId?: string;
+
+  @Column({ type: 'nvarchar', length: 200 })
+  password?: string;
+
+  @Column({ length: 100, nullable: true })
+  uuid?: string;
+
+  @Column({ type: 'nvarchar', length: 100, nullable: true })
+  email?: string;
+
+  @Column({ length: 50, nullable: true })
+  ip?: string;
+
+  @Column({ nullable: true })
+  joinDate?: Date;
+
+  @Column({ nullable: true })
+  lastLogin?: Date;
+}
