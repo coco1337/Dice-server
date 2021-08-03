ca7af342afdb1db09a5b58fae68b04cfbf01becf
ribeKim <ribe@ribe.io>
2021-08-03 11:33:48 +0900

    로그 모듈 추가 & 엔티티 위치 변경

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,8 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { EventsModule } from './events/events.module';
+import { LogsModule } from './logs/logs.module';
 
 @Module({
   imports: [
