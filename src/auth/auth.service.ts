23e9a6e9dc541c499b73ca80b05650d93e20d50f
ribeKim <ribe@ribe.io>
2021-08-03 11:35:27 +0900

    로그인 로그 추가

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { LoginDto } from './dto/login-dto';
-import { User } from '../users/users.entity';
+import { User } from '../users/entities/users.entity';
 
 @Injectable()
 export class AuthService {
@@ -15,7 +15,7 @@ export class AuthService {
     const user = await this.usersService.getAccount(userInfo.username);
 
     if (user && user.password === userInfo.password) {
-      const {password, ...result} = user;
+      const { password, ...result } = user;
       return result;
     }
 
@@ -27,7 +27,7 @@ export class AuthService {
 
     if (!user) return null;
 
-    const payload = {username: user.userId, sub: user.uuid};
+    const payload = { username: user.userId, sub: user.uuid };
 
     return {
       access_token: this.jwtService.sign(payload),
