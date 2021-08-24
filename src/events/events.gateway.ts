78c83a365947e38ee1f7ab13c05135607789273f
ribeKim <ribe@ribe.io>
2021-08-24 19:36:09 +0900

    DiceCore DI 관련, websocket transport 제거

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -6,24 +6,25 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { Server } from 'socket.io';
-import { DiceCoreService } from 'src/dice-core/dice-core.service';
+import { DiceCoreService } from '../dice-core/dice-core.service';
 
-@WebSocketGateway({ cors: 'null,localhost', transports: 'websocket' })
+@WebSocketGateway({ cors: 'null,localhost' })
 export class EventsGateway {
   @WebSocketServer()
   private server: Server;
   private clients = [];
-  private diceCore = new DiceCoreService();
 
-  handleConnection(@ConnectedSocket() client: any) {
-    console.log(`${client.id} connected`);
-    this.clients.push(client);
-  }
+  constructor(private diceCoreService: DiceCoreService) { }
 
-  handleDisconnect(@ConnectedSocket() client: any) {
-    console.log(`${client.id} disconnected`);
-    this.clients.splice(this.clients.indexOf(e => e.id === client.id), 1);  
-  }
+  // handleConnection(@ConnectedSocket() client: any) {
+  //   console.log(`${client.id} connected`);
+  //   this.clients.push(client);
+  // }
+  //
+  // handleDisconnect(@ConnectedSocket() client: any) {
+  //   console.log(`${client.id} disconnected`);
+  //   this.clients.splice(this.clients.indexOf(e => e.id === client.id), 1);
+  // }
 
   @SubscribeMessage('events')
   events(@MessageBody() data: string): string {
@@ -33,6 +34,6 @@ export class EventsGateway {
   @SubscribeMessage('message')
   handleMessage(@ConnectedSocket() client: any, @MessageBody() packet: any): void {
     console.log(`incomming message: ${packet}`);
-    this.diceCore.HandleMessage(packet.data);
+    this.diceCoreService.HandleMessage(packet.data);
   }
 }
